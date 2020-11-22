76ed4bfa4aa4b0c0abd16e74aeb051289360c9cc
Niru Bhandari <nirubhandari45@gmail.com>
2020-11-22 23:51:18 +0545

    Github_clone

---
+++ github_Clone/build/precache-manifest.6a92e183969400a9ebbae0b807118569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5598ef14ab73ed536c1a872466f1c6f3",
+    "url": "/github_clone/index.html"
+  },
+  {
+    "revision": "f606b74c47459fd57767",
+    "url": "/github_clone/static/js/2.a1c91b6b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/github_clone/static/js/2.a1c91b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ba0c3dedbf13c5a023",
+    "url": "/github_clone/static/js/main.2ac4c7a2.chunk.js"
+  },
+  {
+    "revision": "89e41e1ebe67e32a33f4",
+    "url": "/github_clone/static/js/runtime-main.771972d0.js"
+  }
+]);
